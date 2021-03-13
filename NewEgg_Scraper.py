621706bf64654918c13f27c03c8569ca3dc12fee
mktwohy <69822801+mktwohy@users.noreply.github.com>
2021-03-13 01:15:06 -0600

    Overhauled the design for a more OOP approach
    Basic structure: 
    - Page objects are made up of Product objects
    - Data from Products are stored as a list of dictionaries
    - Data is converted to .csv file
    
    Changes:
    - Product object is now fully functioning
    - Can now Pickle data

--- NewEgg_Scraper.py
+++ NewEgg_Scraper.py
@@ -1,110 +1,111 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 12 00:48:50 2021
+Created on Fri Mar 12 22:20:54 2021
 
 @author: Twohy
 """
 
 
 from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup as bSoup
 from time import sleep
 import random
+import pickle
+from os.path import exists
 
-product_specs = []
-
-class product():
-    def __init__(self, spec_list):
-        self.spec_list = spec_list
-        self.model
-        self.brand
-        self.cpu
-
-def get_soup(url):
-    #opening up connection, grabbing the page
-    uClient = uReq(url.replace(' ',''))
-    page_html = uClient.read() # raw html
-    uClient.close()
-    
-    # html parsing
-    return soup(page_html, "html.parser") # parsed html
-
-
-def save_html_data(soup):
-    containers = soup.find_all("div",{"class":"item-container"})
-    numFail = 0
-    for c in containers:
-        try:
-            product_url = c.a['href']
-            details = get_product_data(product_url)
-            print(details)
-            sleep(random.randint(2, 10))
-            product_specs.append(product(details))
-
-            
-            
-        except: 
-            numFail += 1
-            
-    print(numFail,"failed out of",len(containers))
-
-"""Returns list of current page number and last page number"""
-def get_page_num(soup):
-    pagination = soup.findAll("span",{"class":"list-tool-pagination-text"}) 
-    str_list = pagination[0].text.split()[1].split('/')
-    int_list = list(map(int,str_list))
-    return int_list
-
-
-
-def next_page_url(current_url, current_page_num):
-    if '&' in current_url:
-        current_url = current_url.split('&')[0] #remove &page#
-    print(current_page_num)
-    new_url = current_url + "&page=" + str(current_page_num+1)
-    print(new_url)
-    return new_url
-            
-
-
-def save_as_csv(filename):
-    f = open(filename, "w")
-    headers = "brand, product_name, shipping\n"
-    f.write(headers)
-    for i in range(len(brands)):
-        f.write(brands[i] +","+ product_names[i].replace(",","|") +","+ shipping_costs[i]+ "\n")
-
-    f.close()
-    
-def get_product_data(url):
-    product_soup = get_soup(url.replace(' ',''))
-    # details = soup.find_all("div",{'id':'product-details'})
-    tables = product_soup.find_all("table",{'class':'table-horizontal'})
-    rows = list()
-    for i in range(len(tables)):
-        for row in tables[i].findAll("tr"):
-            rows.append(row)
-    
-    for i in range(len(rows)):
-        rows[i] = rows[i].th.text, rows[i].td.text
-    return rows
-
-def scrape(page_url):
-    page_html = get_soup(page_url)
-    page_num, last_page_num = get_page_num(page_html)
-    save_html_data(page_html)
-    
-    if page_num < 3: #last_page_num
-        sleep(random.randint(2, 10))
-        scrape(next_page_url(page_url, page_num))
-
-
-# site URLs
-gtx_url = 'https://www.newegg.com/p/pl?d=gtx+1050&page=2'
-laptop_url = 'https://www.newegg.com/p/pl?d=laptop' 
-xps15_url = 'https://www.newegg.com/p/pl?d=dell+xps+15'
-xps15_product_url = 'https://www.newegg.com/p/1TS-000A-0BMX1?Description=dell xps 15&cm_re=dell_xps 15-_-1TS-000A-0BMX1-_-Product'
+data = list()
+    # def __init__(self, newegg_url):
+    #     self.init_soup = self.get_soup(newegg_url)
+    #     self.products = list()
+    #     self.page = self.Page('https://www.newegg.com/p/pl?d=laptops')
         
-scrape(gtx_url)
-# save_as_csv("laptops.csv")
+def get_soup(url):
+        uClient = uReq(url.replace(' ','')) # opening up connection
+        page_html = uClient.read() # grabbing the page
+        uClient.close() # closing connection
+        
+        return bSoup(page_html, "html.parser") # parse html
+    
+    
+def scrape_page(page_url):
+        # add all products to list  
+        # if page_has_next
+            # scrape_page(next_page)
+        pass
+    
+def save(filename = 'newEgg_Scrape'):
+        pass
+    
+    
+    
+class Page:
+    def __init__(self, page_url):
+        self.soup = get_soup(page_url)
+   
+    def find_next_page():
+        pass
+        
+    
+class Product:
+    def __init__(self,product_url):
+        self.soup = get_soup(product_url)
+        self.all_specs = dict()
+        self.filtered_specs = dict()
+        
+        self.scrape_price()
+        self.scrape_rating()
+        self.scrape_table()
+        self.filter_results()
+        
+        data.append(self.all_specs)
+                            
+    def scrape_table(self):
+        tables = self.soup.find_all("table",{'class':'table-horizontal'})
+        for i in range(len(tables)):
+            for row in tables[i].findAll("tr"):
+                key = row.th.text.strip()
+                val = row.td.text.strip()
+                self.all_specs[key] = val
+
+    #TODO
+    def scrape_rating(self):
+        pass
+    
+    def scrape_price(self):
+        cur_price = self.soup.find_all('li',{'class':'price-current'})[0].text
+        was_price = self.soup.find_all('li',{'class':'price-was'})[0].text
+        
+        if len(was_price) > 0:
+            price = was_price
+        else:
+            price = cur_price
+            
+        self.all_specs['Price'] = price
+
+    def filter_results(self):
+         wanted_specs = ('Brand','Model','Price','CPU')
+         for s in wanted_specs:
+             if s in self.all_specs:
+                 self.filtered_specs[s] = self.all_specs[s]
+
+    def __str__(self):
+        s = ''
+        for k in self.filtered_specs:
+            s = s +'\n'+ k +': ' + self.all_specs[k]
+        return s
+
+def pickle_data(filename = 'PickledData'):
+    filename = filename+"%s"+'.pickle'
+    i = 0
+    while exists(filename % i):
+        i += 1
+    with open(filename % i,'wb') as file:
+        file.write(pickle.dumps(data))
+
+
+plink = 'https://www.newegg.com/carbon-gray-kuu-yobook-workstation/p/1TS-00BK-00049?Item=9SIANGPDNT4057&Description=laptops&cm_re=laptops-_-9SIANGPDNT4057-_-Product&cm_sp=SP-_-444662-_-0-_-0-_-9SIANGPDNT4057-_-laptops-_-laptop-_-4'
+p = Product(plink)
+print(data)
+
+pickle_data()
 
